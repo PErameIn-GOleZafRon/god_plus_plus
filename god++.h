47b7148b6301d16232e0029d26836ec4a6c44725
PErameIn-GOleZafRon <90267145+PErameIn-GOleZafRon@users.noreply.github.com>
2024-02-22 19:44:35 +0400

    Add files via upload

---
+++ god++.h
@@ -0,0 +1,104 @@
+#pragma once
+#define Славься_Исус int main
+#define Жертвоприношение using
+#define	Принятие typedef
+#define Боже std
+#define Божий_голос std::cout
+#define Молитва std::cin
+#define Длинная_молитва(var) std::getline(std::cin,var)
+#define Конец_молвы std::endl
+#define Четвертовать return
+
+#define Служить_богу do
+#define До_поры while
+#define Для for
+
+#define Вопрос_к_богу if
+#define	Ответа_нет else
+
+#define Правда_божия true
+#define Благочестивый_обман false
+
+#define Духовный_сан class
+#define Православный_храм struct 
+#define Перепись enum
+
+#define Отречься break
+#define Продолжить_веру_в_бога continue
+#define	Сменить_веру switch
+#define Все_веры case
+#define Святое default
+
+#define Новая_жизнь new
+#define Смерть_грешника delete
+#define Свет_в_конце_тонеля nullptr
+#define Мир_без_бога NULL
+#define Предначертаное rand
+#define Судьба srand
+#define В_молву to_string
+#define Ширь setw 
+#define Божья_заповедь const
+#define Вознесение_к_святым pow
+#define Господь_тоже_ошибается throw
+#define Исключение_грехов exception
+#define Чтение_библии ifstream
+#define Перепись_библии ofstream
+#define Библия fstream
+#define Святой_человек friend
+#define Переход_по_храмам goto
+#define Пример_поведения template
+#define Своя_религия typename
+#define Божий private
+#define Людской public
+#define Еретикский protected
+#define Внутренний_мир inline
+#define Неподвижный static
+#define Сие this
+#define Попытка_отчистить_грехи try
+#define Ловля_еритиков catch
+#define Наречение namespace
+#define Отворить open
+#define Запереть close
+#define По_божьей_воле auto
+#define Взгляд peek
+#define Близнецы pair
+#define Первенец first
+#define Второй second
+#define Бог operator
+#define Движение_религии move
+#define Мову_небогоугодную_уничтожить setlocale(LC_ALL, "ru"); std::cin.imbue(std::locale("ru_RU.UTF-8"))
+#define Пояснение what
+
+#define Время_от_рождения_христова time
+
+#define Принять_в_дар getline
+#define Божья_благодать good
+#define Божье_презрение fail
+#define Конец_всего eof
+#define Божий_приток ostream 
+#define Отток_божий istream
+#define Смена_тела swap
+#define Получить_подарок get
+
+#define Числецо int
+#define Пол_локтя double
+#define Четверть_локтя float
+#define Православный_алфавит char
+#define Кирилица std::string
+#define Безбожник void
+#define Дитя short
+#define Верующий long
+#define Измеритль_веры size_t
+#define Присутствие_бога bool
+#define Безгрешный unsigned
+
+#define Очередь_к_богу queue
+#define Крайний back
+#define Принять emplace
+#define Отсутствие_веры empty
+#define Первый front
+#define Отсечь_нечастивому pop
+#define Пришить_верующему push
+#define Длинна size
+#define Взять_чашу_святой_воды _Get_container
+#define Переворот static_cast
